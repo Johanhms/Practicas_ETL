fe86907ed2af8e32654df0c6c401297978eb4ea9
Johanhms <johanms1816@gmail.com>
2025-09-01 18:06:07 -0400

    Update Prueba_traspaso_db_postgres.py

--- Prueba_traspaso_db_postgres.py
+++ Prueba_traspaso_db_postgres.py
@@ -6,23 +6,21 @@ import psycopg2
 server = '127.0.0.1'
 db = 'retail_db'
 usr = 'postgres'
-passw = 'Mancity01.'  
 port = '5432'
 tabla_inicial = 'products'
 
-engine = sqlalchemy.create_engine(f"postgresql+psycopg2://{usr}:{passw}@{server}:{port}/{db}")
+engine = sqlalchemy.create_engine(f"postgresql+psycopg2://{usr}:@{server}:{port}/{db}")
 
 
 
 
 server2 = '127.0.0.1'
 db2 = 'test_db'
-usr2 = 'postgres'
-passw2 = 'Mancity01.'  
+usr2 = 'postgres' 
 port2 = '5432'
 tabla_final = 'prodct'
 
-engine2 = sqlalchemy.create_engine(f"postgresql+psycopg2://{usr2}:{passw2}@{server2}:{port2}/{db2}")
+engine2 = sqlalchemy.create_engine(f"postgresql+psycopg2://{usr2}:@{server2}:{port2}/{db2}")
 
 df = pd.read_sql(tabla_inicial, engine.connect())
 
